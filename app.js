879f1baa4acd5e0bce2cafbeda65e1bbd1a3e3f0
Sodha Sohil J. <161869362+Sohil-Sodha@users.noreply.github.com>
2025-07-18 09:35:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path');
 // Global variables
 const app = express();
 // const port = 3000;
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Routes
 const { router: authRoutes } = require('./routes/auth');
